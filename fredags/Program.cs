a5b0bba06549bcdf5f2739387556c328c5d1f77c
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-09-28 18:37:56 +0200

    initial commit

---
+++ fredags/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fredags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 0;
+
+            if (points == 0)
+            {
+                Console.WriteLine("När var spelföretaget Obsidian Entertainment grundat?")
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
